502185712e17be362009c46ec1f26483e22f3bc7
Naz Islam <nazislam@Naz-Als-MacBook-Pro.local>
2018-01-27 19:25:01 -0500

    Created directive search-box.

--- angular-play/angular-hello/src/app/app.module.ts
+++ angular-play/angular-hello/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { OrdersComponent } from './orders/orders.component';
+import { SearchBoxDirective } from './search-box.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
-    OrdersComponent
+    OrdersComponent,
+    SearchBoxDirective
   ],
   imports: [
     BrowserModule
